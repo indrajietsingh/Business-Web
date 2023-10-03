4038caab3849e7d091130ac0812324feb1ff643a
Indrajet <indra@digitizeco.in>
2023-10-03 22:49:16 +0530

    middle step

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Icon from "./Component/Header";
 import Content from "./Component/Content";
 import '../src/index.css'
+import Term from "./Component/Middle ";
 function App(){
   return(
     <>
     <div className="Text">
       <Icon/>
       <Content/>
+      <Term/>
       </div>
     </>
   )
---
+++ src/Component/Middle .jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Middle.css';
+function Term(){
+    return(
+        <>
+        <div className="medium-text">
+            <div className="toggle">
+                <h1>Build a free website in minutes</h1>
+                <p>Create your free website with no credit card required. Or, choose premium plans starting at $3/month.</p>
+                <h3><a class="btn signupPopup" href="/sign-up/">Make My Own Website Now</a></h3>
+            </div>
+            <div className="foggle">
+                
+            </div>
+
+        </div>
+        </>
+    )
+}
+export default Term;
